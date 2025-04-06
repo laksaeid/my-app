ddb5e8d0733826d4bf5c58943b3e1ea3f6612572
laksaeid <laksaeidd2@gmail.com>
2025-04-06 13:01:34 +0330

    initial

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_BASE_URL: "localhost:3000",
+  },
 };
 
 export default nextConfig;
