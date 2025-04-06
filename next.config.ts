18f85ddcbc3301e27fb0c802dd8196b9f45aa9da
laksaeid <laksaeidd2@gmail.com>
2025-04-06 13:11:18 +0330

    delete env

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NEXT_PUBLIC_BASE_URL: "localhost:3000",
-  },
 };
 
 export default nextConfig;
